682544c2304e753e002645d7c98e3a8965fb7bc9
parkermason <120344135+parker-mason@users.noreply.github.com>
2024-01-18 12:17:54 -0700

    Create file_explorer.h

---
+++ file_explorer.h
@@ -0,0 +1,39 @@
+#ifndef FILE_EXPLORER_H
+#define FILE_EXPLORER_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct File File;
+typedef struct Folder Folder;
+tyepdef struct Globals Globals;
+
+struct File
+{
+  char[255] name;
+  char[100] path;
+  uint8_t permissions;
+  char* contents;
+};
+
+struct Folder
+{
+  char[255] name;
+  char[100] path;
+  File files[5];
+  Folder *folders;
+};
+
+struct Globals
+{
+  Folder home;
+  char[100] current_path;
+  char[255] command;
+  char[255] new_thing;
+  char[255] input;
+};
+
+#endif
