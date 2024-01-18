38d2243eea652b9befa47a9935362f988f09c57a
parkermason <120344135+parker-mason@users.noreply.github.com>
2024-01-18 14:27:58 -0700

    Update file_explorer.c WIP

--- file_explorer.c
+++ file_explorer.c
@@ -33,7 +33,7 @@ Folder mkdir( char *name )
 File code( File file )
 {
   system( "clear" );
-  file.contents = fgets( input, sizeof( input ), stdin );
+  file.contents = fgets( globals.input, sizeof( globals.input ), stdin );
   reutrn file;
 }
 
@@ -55,33 +55,33 @@ void run( void )
   while (true)
   {
     printf( "parker@PAKRERMASON%s$ ", globals.current_path );
-    fgets( input, sizeof(input), stdin );
-    input[strcspn( input, "\n" )] = 0;
-    memset( command, 0, strlen( command ) );
-    memset( new_thing, 0, strlen( new_thing ) );
+    fgets( globals.input, sizeof(globals.input), stdin );
+    input[strcspn( globals.input, "\n" )] = 0;
+    memset( globals.command, 0, strlen( globals.command ) );
+    memset( globals.new_thing, 0, strlen( globals.new_thing ) );
 
-    if (strncmp( input, "touch ", 6 ) == 0)
+    if (strncmp( globals.input, "touch ", 6 ) == 0)
     {
       int parser = strlen( TOUCH_COMMAND ) + 1;
-      strcpy( command, TOUCH_COMMAND );
+      strcpy( globals.command, TOUCH_COMMAND );
 
-      for (int i = 0; i < strlen( input ) - strlen( TOUCH_COMMAND ) - 1; i++)
+      for (int i = 0; i < strlen( globals.input ) - strlen( TOUCH_COMMAND ) - 1; i++)
       {
-        new_thing[i] = input[parser];
+        globals.new_thing[i] = globals.input[parser];
         parser++;
       }
 
       if (file_counter < 5)
       {
-        globals.home.files[file_counter] = touch( new_thing, ADMIN_PERMSISSIONS );
+        globals.home.files[file_counter] = touch( globals.new_thing, ADMIN_PERMSISSIONS );
         file_counter++;
       } else
       {
         puts( "ERROR: Max files reached" );
       }
-    } else if (strncmp( input, "ls\0", 2 ))
+    } else if (strncmp( globals.input, "ls\0", 2 ))
     {
-      strcpy( command, LS_COMMAND );
+      strcpy( globals.command, LS_COMMAND );
       for (int i = 0; i < sizeof( globals.home.files ) / sizeof( globals.home.files[0] ); i++)
       {
         if (globals.home.files[j].name != NULL)
@@ -90,12 +90,12 @@ void run( void )
         }
       }
       printf( "\n" );
-    } else if (strncmp( input, "cd ", 3 ))
+    } else if (strncmp( globals.input, "cd ", 3 ))
     {
       int parser = strlen( CD_COMMAND ) + 1;
-      strcpy( command, CD_COMMAND );
+      strcpy( globals.command, CD_COMMAND );
 
-      for (int i = 0; i < strlen( input ) - strlen( CD_COMMAND ) - 1; i++)
+      for (int i = 0; i < strlen( globals.input ) - strlen( CD_COMMAND ) - 1; i++)
       {
 
       }
